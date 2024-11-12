404d3b86825645f6f3d50b13be05290c90f94723
Mohtasim Alam Sohom <94981761+devaoto@users.noreply.github.com>
2024-11-12 16:18:00 +0600

    Format

--- tsup.config.js
+++ tsup.config.js
@@ -1,11 +1,11 @@
-const { defineConfig } = require("tsup");
+const { defineConfig } = require('tsup');
 
 export default defineConfig({
-  entry: ["src/index.ts"],
+  entry: ['src/index.ts'],
   splitting: false,
   sourcemap: true,
   clean: true,
   dts: true,
-  format: ["cjs", "esm"],
+  format: ['cjs', 'esm'],
   minify: true,
 });
