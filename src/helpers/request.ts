c4d16213cf2ce572b9dc10d19b2c0d027bc4f928
Mohtasim Alam Sohom <94981761+devaoto@users.noreply.github.com>
2024-11-14 21:21:12 +0600

    Format

--- src/helpers/request.ts
+++ src/helpers/request.ts
@@ -1,14 +1,14 @@
 export const lilyRequest = async <T>(
   url: URL | string,
   options: RequestInit = {},
-  json = true,
+  json = true
 ) => {
   try {
     const res = await fetch(url, options);
     if (json) {
       return [(await res.json()) as T, undefined] as const;
     }
-      return [(await res.text()) as unknown as T, undefined] as const;
+    return [(await res.text()) as unknown as T, undefined] as const;
   } catch (error) {
     return [undefined, error] as const;
   }
