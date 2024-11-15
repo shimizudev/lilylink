686cfe81c7dab2e8c8119b196e01b6de3159febe
Mohtasim Alam Sohom <94981761+devaoto@users.noreply.github.com>
2024-11-15 17:47:41 +0600

    Use fs instead of json module

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,7 @@
-import pkg from '../package.json';
+import { readFileSync } from 'node:fs';
+import { join } from 'node:path';
 
+const pkg = JSON.parse(
+  readFileSync(join(__dirname, '..', 'package.json'), 'utf-8')
+) as { version: string };
 export const version = pkg.version || 'unknown';
